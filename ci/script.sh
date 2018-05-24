98e97dd60ea13819c5dbe5a8d4301ece1f2c6d29
Rudi Horn <dyn-git@rudi-horn.de>
2018-05-24 13:15:25 +0100

    fixes to ci

--- ci/script.sh
+++ ci/script.sh
@@ -2,7 +2,16 @@ set -euxo pipefail
 
 main() {
     cargo check --target $TARGET
-    cargo check --target $TARGET --example rpi
+
+    if [ $TRAVIS_BRANCH = extra_examples ]; then
+        if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+            cargo check --example --target $TARGET --example rpi
+        elif [ $TARGET = thumbv7m-none-eabi ]; then
+            cargo check --example --target $TARGET --example stm32
+            cargo check --example --target $TARGET --example stm32_ssd1306
+        fi
+    fi
+
     cargo test --target $TARGET
 }
 
