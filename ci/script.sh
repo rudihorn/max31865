d4e4a8c4ca57cbe4f6271add1e51227674f972ae
Rudi Horn <dyn-git@rudi-horn.de>
2018-05-24 13:35:05 +0100

    test only on x86

--- ci/script.sh
+++ ci/script.sh
@@ -12,7 +12,11 @@ main() {
         fi
     fi
 
-    cargo test --target $TARGET
+    if [ $TARGET = x86_64-unknown-linux-gnu ]; then 
+        # the --tests is required to ignore the examples
+        # which will not compile under x86
+        cargo test --target $TARGET
+    fi
 }
 
 main
